de593e3d24ee8519d9d6ed7e4460a6d5e302917f
nikilasilva <nikilasilva@gmail.com>
2025-08-24 23:44:36 +0530

    style: add retro loading dots and loading text for name_registration_screen

--- lib/features/auth/presentation/screens/name_registration_screen.dart
+++ lib/features/auth/presentation/screens/name_registration_screen.dart
@@ -2,7 +2,7 @@ import 'package:chat_drop/core/theme/app_colors.dart';
 import 'package:chat_drop/core/theme/app_text_styles.dart';
 import 'package:chat_drop/core/utils/retro_snackbar.dart';
 import 'package:chat_drop/core/widgets/retro_button.dart';
-import 'package:chat_drop/core/widgets/retro_loading_indicator.dart';
+import 'package:chat_drop/core/widgets/retro_typing_dots.dart';
 import 'package:chat_drop/features/auth/presentation/providers/auth_providers.dart';
 import 'package:chat_drop/features/auth/widgets/retro_text_field.dart';
 import 'package:flutter/material.dart';
@@ -79,94 +79,105 @@ class _NameRegistrationScreenState
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.background,
-      body: SafeArea(
-        child: Container(
-          width: double.infinity,
-          height: double.infinity,
-          margin: const EdgeInsets.all(20.0),
-          decoration: BoxDecoration(
-            color: AppColors.retroPink,
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(color: AppColors.border, width: 2),
-          ),
-          child: SingleChildScrollView(
-            child: ConstrainedBox(
-              constraints: BoxConstraints(
-                minHeight:
-                    MediaQuery.of(context).size.height -
-                    40 -
-                    MediaQuery.of(context).padding.top -
-                    MediaQuery.of(context).padding.bottom,
+    return Stack(
+      children: [
+        Scaffold(
+          backgroundColor: AppColors.background,
+          body: SafeArea(
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              margin: const EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                color: AppColors.retroPink,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: AppColors.border, width: 2),
               ),
-              child: IntrinsicHeight(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 20.0,
-                    vertical: 40.0,
+              child: SingleChildScrollView(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                    minHeight:
+                        MediaQuery.of(context).size.height -
+                        40 -
+                        MediaQuery.of(context).padding.top -
+                        MediaQuery.of(context).padding.bottom,
                   ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(height: 10),
-
-                      // Logo Section
-                      SizedBox(
-                        width: 120,
-                        height: 120,
-                        child: SvgPicture.asset(
-                          'assets/images/logo.svg',
-                          fit: BoxFit.contain,
-                        ),
+                  child: IntrinsicHeight(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20.0,
+                        vertical: 40.0,
                       ),
-                      const SizedBox(height: 6),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const SizedBox(height: 10),
 
-                      // App Name
-                      const Text('ChatDrop', style: AppTextStyles.headingXL),
-                      const SizedBox(height: 60),
+                          // Logo Section
+                          SizedBox(
+                            width: 120,
+                            height: 120,
+                            child: SvgPicture.asset(
+                              'assets/images/logo.svg',
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                          const SizedBox(height: 6),
 
-                      // Middle section
-                      const SizedBox(
-                        width: 250,
-                        child: Text(
-                          'Pick a Name to Drop In',
-                          style: AppTextStyles.heading,
-                          textAlign: TextAlign.center,
-                        ),
+                          // App Name
+                          const Text(
+                            'ChatDrop',
+                            style: AppTextStyles.headingXL,
+                          ),
+                          const SizedBox(height: 60),
+
+                          // Middle section
+                          const SizedBox(
+                            width: 250,
+                            child: Text(
+                              'Pick a Name to Drop In',
+                              style: AppTextStyles.heading,
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+
+                          // Name input section
+                          SizedBox(
+                            width: 250,
+                            child: RetroTextField(
+                              controller: nameController,
+                              hintText: 'Enter a nickname...',
+                            ),
+                          ),
+
+                          const Expanded(child: SizedBox()),
+
+                          // Register button section
+                          RetroButton(
+                            text: "Join the chat  >",
+                            onPressed: _isLoading ? null : _registerUser,
+                            backgroundColor: AppColors.accentPink,
+                          ),
+                        ],
                       ),
-                      const SizedBox(height: 20),
-
-                      // Name input section
-                      SizedBox(
-                        width: 250,
-                        child: RetroTextField(
-                          controller: nameController,
-                          hintText: 'Enter a nickname...',
-                        ),
-                      ),
-
-                      const Expanded(child: SizedBox()),
-
-                      // Register button section
-                      RetroButton(
-                        text: "Join the chat  >",
-                        onPressed: _isLoading ? null : _registerUser,
-                        backgroundColor: AppColors.accentPink,
-                        width: double.infinity,
-                        child:
-                            _isLoading
-                                ? const RetroLoadingIndicator(size: 8)
-                                : null,
-                      ),
-                    ],
+                    ),
                   ),
                 ),
               ),
             ),
           ),
         ),
-      ),
+
+        // Retro Loading Overlay
+        if (_isLoading)
+          Container(
+            color: Colors.black.withOpacity(0.7),
+            child: const Center(
+              child: RetroTypingDots(dotSize: 15, dotCount: 4, loadingText: 'Loading',),
+            ),
+          ),
+      ],
     );
   }
 }
